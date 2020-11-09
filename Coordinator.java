4bb308491357637145f2cb7fa2fcb2bf10238e97
ssankha <ssankha@purdue.edu>
2020-11-09 17:32:07 -0500

    showMenu(), addJob(), removeJob(), printSchedule()
    Program now loops until user chooses to exits. removeJob() and printSchedule() have been coded for the corner case if the user has not added any jobs. removeJob() has been coded in case the name of the job the user inputs cannot be found.

--- Coordinator.java
+++ Coordinator.java
@@ -9,12 +9,32 @@ public class Coordinator {
     public static void main(String[] args) {
 
         // initialize scanner
-
+        Scanner scan = new Scanner(System.in);
 
         // print menu
+        boolean runAgain = false;
 
+        do {
+            runAgain = true;
 
-        // actions based on menu selection
+            showMenu();
+
+            String choice = scan.nextLine();
+
+            if (choice.equals("1")) {
+                addJob(scan);
+            } else if (choice.equals("2")) {
+                removeJob(scan);
+            } else if (choice.equals("3")) {
+                printSchedule();
+            } else if (choice.equals("4")) {
+                System.out.println("Now exiting the Job Scheduler...");
+                runAgain = false;
+            } else {
+                System.out.println("Please select a valid option!");
+            }
+        } while (runAgain);
+
 
     }
 
@@ -29,53 +49,85 @@ public class Coordinator {
         int duration = Integer.parseInt(scan.nextLine());
 
         jobs.add(new Job(name, deadline, duration));
+
+        computeSchedule();
     }
 
     public static void removeJob(Scanner scan) {
+
+        if (jobs.size() == 0) {
+            System.out.println("There are no jobs to remove!");
+
+            return;
+        }
+
         System.out.println("What is the name of the job you want to remove?");
         String name = scan.nextLine();
 
         boolean found = false;
         int index = -1;
 
-        if(jobs.get(0) != null) {
 
-            for(Job j : jobs) {
-                if(j.getName().equals(name)) {
-                    found = true;
-                    jobs.remove(j);
+        for (Job j : jobs) {
 
-                    break;
-                }
+            if (j.getName().equals(name)) {
+                found = true;
+                jobs.remove(j);
+
+                break;
             }
         }
 
-        if(found) {
+        computeSchedule();
+
+
+        if (found) {
             System.out.println("Job has been removed!");
-        }
-        else {
-            System.out.println("A job with name was not found!");
+        } else {
+            System.out.println("A job with that name was not found!");
         }
 
     }
 
-    public static void printSchedule(){
+    public static void printSchedule() {
 
+        if (jobs.size() == 0) {
+            System.out.println("There are no jobs to display!");
+
+            return;
+        }
+
+        int counter = 0;
+
+        for (Job j : jobs) {
+            System.out.println(++counter + ") " + j.getName());
+        }
     }
 
     public static void computeSchedule() {
-        if(jobs.get(0) == null) {
+        if (jobs.size() == 0) {
             System.out.println("No jobs to create schedule!");
 
             return;
         }
 
-        // sort jobs by descending order
+        // sort jobs based on deadline (with the longest ones at the end)
         Collections.sort(jobs);
+
+        // initialize a temp[] to hold the jobs
+
+
 
     }
 
     public static void showMenu() {
-
+        System.out.println("***********************");
+        System.out.println("Welcome to the Job Scheduler!");
+        System.out.println("Please select from one of the options below");
+        System.out.println("1) Add a job");
+        System.out.println("2) Remove a job");
+        System.out.println("3) Print out the current schedule");
+        System.out.println("4) Exit");
+        System.out.println("***********************");
     }
 }
